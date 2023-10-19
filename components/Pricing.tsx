76c11e9c1f39fa5cf27fd3da28d9fdc3958025e0
Anusheel Bhushan <anusheel@users.noreply.github.com>
2023-10-19 01:21:05 -0700

    Commit from Stub AI (#2)

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -17,20 +17,31 @@ const Pricing: React.FC<PricingProps> = ({ plans }) => {
   const [isAnnual, setIsAnnual] = useState(false);
 
   return (
-    <div>
-      <button onClick={() => setIsAnnual(!isAnnual)}>
+    <div className="flex flex-wrap justify-center">
+      <button 
+        className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setIsAnnual(!isAnnual)}
+      >
         {isAnnual ? 'Switch to Monthly' : 'Switch to Annual'}
       </button>
       {plans.map((plan, index) => (
-        <div key={index}>
-          <h2>{plan.name}</h2>
-          <p>{isAnnual ? plan.priceAnnual : plan.priceMonthly} per {isAnnual ? 'year' : 'month'}</p>
-          <ul>
-            {plan.features.map((feature, index) => (
-              <li key={index}>{feature}</li>
-            ))}
-          </ul>
-          <button>Sign Up</button>
+        <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{plan.name}</div>
+            <p className="text-gray-700 text-base">
+              {isAnnual ? plan.priceAnnual : plan.priceMonthly} per {isAnnual ? 'year' : 'month'}
+            </p>
+            <ul className="list-disc pl-5">
+              {plan.features.map((feature, index) => (
+                <li key={index} className="py-1">{feature}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Sign Up
+            </button>
+          </div>
         </div>
       ))}
     </div>
